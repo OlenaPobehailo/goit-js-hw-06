c5ff300b594c2b7e43d6c63957cc30fcf160d1c7
OlenaPobehailo <olena.pobehailo@gmail.com>
2022-11-26 22:07:02 +0200

    add tasks 5, 6

---
+++ js/task-05.js
@@ -0,0 +1,22 @@
+/*
+Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
+
+<input type="text" id="name-input" placeholder="Please enter your name" />
+<h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+*/
+
+const refs = {
+    nameInput: document.querySelector('#name-input'),
+    nameOutput: document.querySelector('#name-output'),
+};
+
+function onNameInputChange(event) {
+    if (event.currentTarget.value === '') {
+        refs.nameOutput.textContent = 'Anonymous';
+    } else {
+        refs.nameOutput.textContent = event.currentTarget.value;
+    }
+}
+
+refs.nameInput.addEventListener('input', onNameInputChange);
---
+++ js/task-06.js
@@ -0,0 +1,29 @@
+/*
+Напиши скрипт, який під час втрати фокусу на інпуті (подія blur), перевіряє його вміст щодо правильної кількості введених символів.
+
+<input
+  type="text"
+  id="validation-input"
+  data-length="6"
+  placeholder="Please enter 6 symbols"
+/>
+
+Яка кількість символів повинна бути в інпуті, зазначається в його атрибуті data-length.
+Якщо введена правильна кількість символів, то border інпуту стає зеленим, якщо неправильна кількість - червоним.
+Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
+
+*/
+const validationInput = document.querySelector('#validation-input');
+const dataLength = Number(validationInput.dataset.length);
+
+function onValidationInputBlur(event) {
+    if (event.currentTarget.value.length === dataLength) {
+        validationInput.classList.add('valid');
+        validationInput.classList.remove('invalid');
+    } else {
+        validationInput.classList.add('invalid');
+        validationInput.classList.remove('valid');
+    }
+}
+
+validationInput.addEventListener('blur', onValidationInputBlur);
